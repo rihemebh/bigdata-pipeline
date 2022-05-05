61bedd65b7f7c57ec8ba223b67a9fba83e9c5a5f
rihemebenhassan <rihemebenhassan@insat.u-carthage.tn>
2022-05-05 18:50:40 +0100

    kafka integration

--- pipeline/src/main/java/ColumnRefactorTask.java
+++ pipeline/src/main/java/ColumnRefactorTask.java
@@ -1,19 +1,25 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import static jersey.repackaged.com.google.common.base.Preconditions.checkArgument;
 
+import org.apache.kafka.clients.producer.Producer;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.StringJoiner;
 
 public class ColumnRefactorTask {
+    public final static String TOPIC_NAME = "Bigdata_pipeline";
 
-    public String joinRow(String[] tab){
+    public String joinRow(String[] tab) {
         StringJoiner sj = new StringJoiner(",");
-        for(String s:tab) {
-            if(s != null)
+        for (String s : tab) {
+            if (s != null)
                 sj.add(s);
         }
         return String.valueOf(sj);
@@ -21,46 +27,63 @@ public class ColumnRefactorTask {
 
 
     public static void main(String[] args) {
-        checkArgument(args.length > 1, "Please provide the path of input file and output dir as parameters.");
-        new ColumnRefactorTask().run(args[0], args[1]);
+        checkArgument(args.length > 1, "Please provide the path of input file as parameters.");
+        new ColumnRefactorTask().run(args[0]);
     }
 
 
-    public void run(String inputFilePath, String outputDir) {
+    public void run(String inputFilePath) {
 
-        String master = "local[*]";
-        /**
-         * Don't forget to remove the setMaster config when you want to try it in the clusterZ
-         */
         SparkConf conf = new SparkConf()
-                .setAppName(ColumnRefactorTask.class.getName())
-        .setMaster(master);
+                .setAppName(ColumnRefactorTask.class.getName());
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> textFile = sc.textFile(inputFilePath);
 
         List<String> rows = textFile.collect();
         List<String> newRows = new ArrayList<>();
-        for (String row: rows
-             ) {
-            String [] a = row.split(",");
-            if(a[a.length - 1].endsWith("\""))
-            {
-                a[a.length-2] = a[a.length-2]+'.'+a[a.length-1];
-                a[a.length-1] = null;
+        for (String row : rows
+        ) {
+            String[] a = row.split(",");
+            if (a[a.length - 1].endsWith("\"")) {
+                a[a.length - 2] = a[a.length - 2] + '.' + a[a.length - 1];
+                a[a.length - 1] = null;
 
                 newRows.add(joinRow(a));
-            }
-            else if(a[a.length - 1].equals("C")) {
+            } else if (a[a.length - 1].equals("C")) {
                 a[a.length - 1] = "0";
 
                 newRows.add(joinRow(a));
-            }else{
+            } else {
                 newRows.add(row);
             }
 
         }
-        JavaRDD<String> rdd = sc.parallelize(newRows);
 
-        rdd.saveAsTextFile(outputDir);
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+
+        props.put("acks", "all");
+
+        props.put("retries", 0);
+
+        props.put("batch.size", 16384);
+
+        props.put("buffer.memory", 33554432);
+
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer
+                <String, String>(props);
+
+        for (String newRow : newRows) {
+            producer.send(new ProducerRecord<String, String>(TOPIC_NAME,
+                    newRow));
+
+        }
+        System.out.println("Dataset sent seccussfully");
+        producer.close();
     }
 }
